4add533b38b01338e8741d73c0c06c616907018c
Katsumi Shimotsu <shimotsu@e.u-tokyo.ac.jp>
2018-01-16 14:19:15 +0900

    Allow rsubgroup() to generate data even when m=1.

--- R/rsubgroup.R
+++ R/rsubgroup.R
@@ -10,22 +10,28 @@ rsubgroup <- function (n, tau, mubeta, sigma, x, v) {
   # Output
   #  y : n by 1 vector
   m <- ncol(mubeta)
-  q <- length(tau)
-  mubeta   <- matrix(mubeta, ncol=m)
-  tau <- matrix(tau, nrow = q)
-
   v <- as.matrix(v)
   x <- as.matrix(x)
   v1 <- cbind(1, v)
   x1 <- cbind(1, x)
 
-  v1tau <- v1 %*% tau
-  alpha1 <- exp(v1tau) / (1 + exp(v1tau))
-  alpha <- cbind(alpha1, 1 - alpha1)
-  ii <- apply(alpha, 1, function(x) {sample(m, 1, replace = TRUE, prob = x)})
+  if (m > 1){
+    q <- length(tau)
+    mubeta   <- matrix(mubeta, ncol=m)
+    tau <- matrix(tau, nrow = q)
 
-  x1mubeta <- rowSums(x1 * t(mubeta[, ii]))
-  y <- rnorm(n = n, mean = x1mubeta, sd=sigma[ii])
+    v1tau <- v1 %*% tau
+    alpha1 <- exp(v1tau) / (1 + exp(v1tau))
+    alpha <- cbind(alpha1, 1 - alpha1)
+    ii <- apply(alpha, 1, function(x) {sample(m, 1, replace = TRUE, prob = x)})
+
+    x1mubeta <- rowSums(x1 * t(mubeta[, ii]))
+    y <- rnorm(n = n, mean = x1mubeta, sd=sigma[ii])
+  } else {
+    mubeta   <- matrix(mubeta, ncol=1)
+    x1mubeta <- x1 %*% mubeta
+    y <- rnorm(n = n, mean = x1mubeta, sd=sigma)
+  }
 
   y
 
